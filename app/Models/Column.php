cd7d6499ff60623257043c4320660fdf2aa48afb
Jimmy <jimmy@datafortune.com>
2024-05-19 00:51:38 +0530

    Added option to change column order

--- app/Models/Column.php
+++ app/Models/Column.php
@@ -15,7 +15,18 @@ class Column extends Model
     protected $fillable = [
         'title',
         'board_id',
+        'order'
     ];
+
+    // onsaving the column, fill order with the last column position, grouped by board_id
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($column) {
+            $column->order = static::where('board_id', $column->board_id)->max('order') + 1;
+        });
+    }
 
     public function board(): BelongsTo
     {
@@ -29,5 +40,29 @@ class Column extends Model
 
     public function addCard(Card $card): void{
         $this->cards()->save($card);
+    }
+
+    /**
+     * Move the column to the specified direction by one step by updating order 
+     * 
+     * @param string $direction left or right 
+     * 
+     * @return void
+     */
+    public function move(string $direction){
+        $currentOrder = $this->order;
+        $newOrder = $direction === 'left' ? $currentOrder - 1 : $currentOrder + 1;
+
+        $columnToSwap = static::where('board_id', $this->board_id)
+            ->where('order', $newOrder)
+            ->first();
+
+        if($columnToSwap){
+            $this->order = $newOrder;
+            $columnToSwap->order = $currentOrder;
+
+            $this->save();
+            $columnToSwap->save();
+        }
     }
 }
