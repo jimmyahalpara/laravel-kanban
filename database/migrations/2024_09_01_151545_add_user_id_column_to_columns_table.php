dd6489a9705be5833c5f918dd0b3eda5188cd062
Jimmy <jimmy@datafortune.com>
2024-09-01 21:16:25 +0530

    Update in migration

--- database/migrations/2024_09_01_151545_add_user_id_column_to_columns_table.php
+++ database/migrations/2024_09_01_151545_add_user_id_column_to_columns_table.php
@@ -12,7 +12,9 @@ return new class extends Migration
     public function up(): void
     {
         Schema::table('columns', function (Blueprint $table) {
-            $table->foreignId('user_id')->constrained();
+            if (!Schema::hasColumn('columns', 'user_id')) {
+                $table->foreignId('user_id')->constrained();
+            }
         });
     }
 
